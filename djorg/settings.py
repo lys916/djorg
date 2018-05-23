32564b5fe3999c32646cffec59fa247fc56f87ec
Lo Saephan <teamsky916@gmail.com>
2018-05-23 14:42:40 -0700

    progress commit - added .env file - added bookmarks url route - added bookmarks template

--- djorg/settings.py
+++ djorg/settings.py
@@ -11,19 +11,19 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=cq6n=mkd23$u)g6%ub=xkq-4ue!u4iwg%-fh)@=!0-z6ckfyq'
-
+# SECRET_KEY = '=cq6n=mkd23$u)g6%ub=xkq-4ue!u4iwg%-fh)@=!0-z6ckfyq'
+DEBUG = config('DEBUG', cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
