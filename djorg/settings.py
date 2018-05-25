c505ec97bb8943ff75248bd0d2f31eacf3221c1e
Lo Saephan <teamsky916@gmail.com>
2018-05-25 10:48:31 -0700

    added dj_database_url.config in setting.py

--- djorg/settings.py
+++ djorg/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from decouple import config, dj_database_url
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -120,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
